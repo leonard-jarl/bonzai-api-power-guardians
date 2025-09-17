22dae522c5184fd4741305e4cde37e58a3a70a18
Leonard <leonard.jarl@hotmail.com>
2025-09-17 09:58:36 +0200

    added error handling

--- functions/GetBookings/index.js
+++ functions/GetBookings/index.js
@@ -4,32 +4,44 @@ import { unmarshall } from "@aws-sdk/util-dynamodb";
 const client = new DynamoDBClient({ region: "eu-north-1" });
 
 export const handler = async (event) => {
-  const command = new QueryCommand({
-    TableName: "bonzaiAPI",
-    KeyConditionExpression: "pk = :pk",
-    ExpressionAttributeValues: {
-      ":pk": { S: "BOOKINGS" },
+  try {
+    const command = new QueryCommand({
+      TableName: "bonzaiAPI",
+      KeyConditionExpression: "pk = :pk",
+      ExpressionAttributeValues: {
+        ":pk": { S: "BOOKINGS" },
+      }
+    });
+
+    const { Items = [] } = await client.send(command);
+    const bookings = Items.map((item) => unmarshall(item));
+
+    if (bookings.length === 0) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Det finns inga bokningar just nu.",
+          bookings: []
+        }),
+      };
     }
-  });
 
-  const { Items = [] } = await client.send(command);
-
-  const bookings = Items.map((item) => unmarshall(item));
-
-  if (bookings.length === 0) {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: `Det finns inga bokningar just nu.`,
+        message: "Det funkar! Här är alla nuvarande bokningar",
+        bookings,
+      }),
+    };
+  } catch (error) {
+    console.error("Ett fel uppstod vid hämtning av bokningar:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Ett fel uppstod vid hämtning av bokningar.",
+        error: error.message,
       }),
     };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Det funkar! Här är alla nuvarande bokningar`,
-      bookings: bookings,
-    }),
-  };
 };
